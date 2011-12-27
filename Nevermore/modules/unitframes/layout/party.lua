c21d241d3a18fa826cce7b7cf7dd3f5bfb62df6a
DigiDaemon <suisilverman@gmail.com>
2011-12-27 05:28:14 -0500

    - (Alteration) changed location of Auction Progress Frame to Bottom Right of Auction Frame. - (Coding) Fixing Debuff Highlighing to include icons.

--- Nevermore/modules/unitframes/layout/party.lua
+++ Nevermore/modules/unitframes/layout/party.lua
@@ -1,11 +1,10 @@
+local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
 local ADDON_NAME, ns = ...
 local oUF = ns.oUF or oUF
 assert(oUF, "Nevermore was unable to locate oUF install.")
 
 ns._Objects = {}
 ns._Headers = {}
-
-local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
 
 if not C["unitframes"].enable == true then return end
 
@@ -17,25 +16,22 @@ local function Shared(self, unit)
 	self:RegisterForClicks("LeftButtonDown", "RightButtonDown")
 	self:SetScript('OnEnter', UnitFrame_OnEnter)
 	self:SetScript('OnLeave', UnitFrame_OnLeave)
-
 	self:SetAttribute('type2', 'menu')
-    
 	self:SetBackdrop({bgFile = "Interface\\Buttons\\WHITE8x8", insets = {top = -1, left = -1, bottom = -1, right = -1}})
 	self:SetBackdropColor(C["general"].backdropcolor)    
 -----------------------------------------------------------------------------
 -- health
 -----------------------------------------------------------------------------
-    local health = CreateFrame('StatusBar', nil, self)
-    health:SetPoint("TOPLEFT")
+local health = CreateFrame('StatusBar', nil, self)
+	health:SetPoint("TOPLEFT")
 	health:SetPoint("TOPRIGHT")
 	health:SetHeight(22)
 	health:SetStatusBarTexture(C["media"].normTex)
-    health:SetStatusBarColor(0,0,0)
-    
-    health.frequentUpdates = true
-    health.colorDisconnected = false
-    health.colorClass = false
-    health.Smooth = true
+	health:SetStatusBarColor(0,0,0)
+	health.frequentUpdates = true
+	health.colorDisconnected = false
+	health.colorClass = false
+	health.Smooth = true
 	self.Health = health
 -----------------------------------------------------------------------------
 -- healthbackground
@@ -45,105 +41,101 @@ local function Shared(self, unit)
 	health.bg:SetTexture(C["media"].normTex)
 	health.bg:SetVertexColor(.4,.4,.4)
 	health.bg.multiplier = 0.3
-    self.Health.bg = health.bg
+	self.Health.bg = health.bg
 -----------------------------------------------------------------------------
 -- Panels
 -----------------------------------------------------------------------------
 -----------------------------------------------------------------------------
 -- Healthborder
 -----------------------------------------------------------------------------
-    local healthborder = CreateFrame("frame","healthborder",health)
-    healthborder:SetBackdrop({
-        bgFile = "Interface\\Buttons\\WHITE8x8", 
-        edgeFile = "Interface\\Buttons\\WHITE8x8", 
-        tile = false,
-        tileSize = 0,
-        edgeSize = 1, 
-        insets = {
-        left = -1,
-        right = -1,
-        top = -1,
-        bottom = -1
-        }
-    });
-    healthborder:SetBackdropColor(0, 0, 0, 1);
-    healthborder:SetBackdropBorderColor(0.5, 0.5, 0.5);
-    healthborder:SetPoint("TOPLEFT",-2,2)
-    healthborder:SetPoint("BOTTOMRIGHT",2,-2)
-    healthborder:SetBackdropColor(0,0,0,1)
-    healthborder:SetFrameLevel(self:GetFrameLevel())
-    healthborder:SetFrameStrata(self:GetFrameStrata())
+	local healthborder = CreateFrame("frame","healthborder",health)
+	healthborder:SetBackdrop({
+	        bgFile = "Interface\\Buttons\\WHITE8x8", 
+	        edgeFile = "Interface\\Buttons\\WHITE8x8", 
+	        tile = false,
+	        tileSize = 0,
+	        edgeSize = 1, 
+	        insets = {
+		        left = -1,
+		        right = -1,
+		        top = -1,
+		        bottom = -1
+		        }
+		    });
+	healthborder:SetBackdropColor(0, 0, 0, 1);
+	healthborder:SetBackdropBorderColor(0.5, 0.5, 0.5);
+	healthborder:SetPoint("TOPLEFT",-2,2)
+	healthborder:SetPoint("BOTTOMRIGHT",2,-2)
+	healthborder:SetBackdropColor(0,0,0,1)
+	healthborder:SetFrameLevel(self:GetFrameLevel())
+	healthborder:SetFrameStrata(self:GetFrameStrata())
 -----------------------------------------------------------------------------
 -- Panel at the bottom
 -----------------------------------------------------------------------------
-    self.Panel = CreateFrame("Frame", self:GetName().."Panel", health)
-    self.Panel:SetPoint("TOP", health, "BOTTOM", 0, -1)
-    self.Panel:SetSize(self:GetWidth()+4, 17)
-    self.Panel:SetBackdrop({
-        bgFile = "Interface\\Buttons\\WHITE8x8", 
-        edgeFile = "Interface\\Buttons\\WHITE8x8", 
-        tile = false,
-        tileSize = 0,
-        edgeSize = 1, 
-        insets = {
-        left = -1,
-        right = -1,
-        top = -1,
-        bottom = -1
-        }
-    });
-    self.Panel:SetBackdropColor(0, 0, 0, 1);
-    self.Panel:SetBackdropBorderColor(0.5, 0.5, 0.5);
-    self.Panel:SetFrameLevel(self:GetFrameLevel())
-    self.Panel:SetFrameStrata(self:GetFrameStrata())
-    
-    local bg = self.Panel:CreateTexture(nil, 'BORDER')
-    bg:SetPoint("TOPLEFT", 2, -2)
-    bg:SetPoint("BOTTOMRIGHT", -2, 2)
-    bg:SetTexture(C["media"].normTex);
-    bg:SetVertexColor(0, 0, 0)
-
+	self.Panel = CreateFrame("Frame", self:GetName().."Panel", health)
+	self.Panel:SetPoint("TOP", health, "BOTTOM", 0, -1)
+	self.Panel:SetSize(self:GetWidth()+4, 17)
+	self.Panel:SetBackdrop({
+	        bgFile = "Interface\\Buttons\\WHITE8x8", 
+	        edgeFile = "Interface\\Buttons\\WHITE8x8", 
+	        tile = false,
+	        tileSize = 0,
+	        edgeSize = 1, 
+	        insets = {
+			left = -1,
+			right = -1,
+			top = -1,
+			bottom = -1
+			}
+		});
+	self.Panel:SetBackdropColor(0, 0, 0, 1);
+	self.Panel:SetBackdropBorderColor(0.5, 0.5, 0.5);
+	self.Panel:SetFrameLevel(self:GetFrameLevel())
+	self.Panel:SetFrameStrata(self:GetFrameStrata())
+local bg = self.Panel:CreateTexture(nil, 'BORDER')
+	bg:SetPoint("TOPLEFT", 2, -2)
+	bg:SetPoint("BOTTOMRIGHT", -2, 2)
+	bg:SetTexture(C["media"].normTex);
+	bg:SetVertexColor(0, 0, 0)
 -----------------------------------------------------------------------------
 -- Power
 -----------------------------------------------------------------------------
-	local power = CreateFrame('StatusBar', nil, self);
-		power:SetStatusBarTexture(C["media"].normTex);
-		power:SetHeight(self.Panel:GetHeight())
-    		power:SetWidth( self.Panel:GetWidth() )
-   		power:SetPoint("TOPLEFT", self.Panel, 2, -2)
-		power:SetPoint("BOTTOMRIGHT", self.Panel, -2, 2)
-		self.Power = power
-
+local power = CreateFrame('StatusBar', nil, self);
+	power:SetStatusBarTexture(C["media"].normTex);
+	power:SetHeight(self.Panel:GetHeight())
+	power:SetWidth( self.Panel:GetWidth() )
+	power:SetPoint("TOPLEFT", self.Panel, 2, -2)
+	power:SetPoint("BOTTOMRIGHT", self.Panel, -2, 2)
+	self.Power = power
 -----------------------------------------------------------------------------
 -- Power Background
 -----------------------------------------------------------------------------
-	local powerBG = power:CreateTexture(nil, 'BORDER')
-		powerBG:SetAllPoints(power);
-		powerBG:SetTexture(C["media"].normTex);
-		powerBG.multiplier = 0.5
-		self.Power.bg = powerBG
-
+local powerBG = power:CreateTexture(nil, 'BORDER')
+	powerBG:SetAllPoints(power);
+	powerBG:SetTexture(C["media"].normTex);
+	powerBG.multiplier = 0.5
+	self.Power.bg = powerBG
 -----------------------------------------------------------------------------
 -- Power functions
 ----------------------------------------------------------------------------- 
-		power.frequentUpdates = 0.1 -- (for rogues and cats)
-		power.Smooth = true 
-		power.colorDisconnected = true
-		power.colorClass = true
-		power.colorClassNPC = false
-		power.colorTapping = true	
+	power.frequentUpdates = 0.1 -- (for rogues and cats)
+	power.Smooth = true 
+	power.colorDisconnected = true
+	power.colorClass = true
+	power.colorClassNPC = false
+	power.colorTapping = true	
 -----------------------------------------------------------------------------
 -- Leader
 -----------------------------------------------------------------------------
-	local Leader = self.Health:CreateTexture(nil, "OVERLAY")
-		Leader:SetHeight(14);
-		Leader:SetWidth(14);
-		Leader:SetPoint("LEFT", power, "LEFT", 2, 1);
-		self.Leader = Leader
+local Leader = self.Health:CreateTexture(nil, "OVERLAY")
+	Leader:SetHeight(14);
+	Leader:SetWidth(14);
+	Leader:SetPoint("LEFT", power, "LEFT", 2, 1);
+	self.Leader = Leader
 -----------------------------------------------------------------------------
 -- Targetborder
 -----------------------------------------------------------------------------
-    self.Border = CreateFrame("Frame", nil, self);
+self.Border = CreateFrame("Frame", nil, self);
 	self.Border:SetBackdrop({
 		bgFile = "Interface\\Buttons\\WHITE8x8",
 		edgeFile = "Interface\\Buttons\\WHITE8x8",
@@ -165,72 +157,56 @@ local function Shared(self, unit)
 	self.Border:SetBackdropColor(0,0,0,0);
 	self.Border:Hide();
 -----------------------------------------------------------------------------
--- healthvalue
------------------------------------------------------------------------------
---	health.value = SetFontString(health)
---	health.value:SetPoint("RIGHT", self.Panel, "RIGHT", -4, 0);
---	health.PostUpdate = PostUpdateHealthRaid
------------------------------------------------------------------------------
 -- Role Icon
 -----------------------------------------------------------------------------
-	local LFDRole = self.Health:CreateTexture(nil, "OVERLAY")
+local LFDRole = self.Health:CreateTexture(nil, "OVERLAY")
 	LFDRole:SetHeight(19)
 	LFDRole:SetWidth(19)
 	LFDRole:SetPoint("LEFT", 2, -1)
 	LFDRole:Hide()
 	self.LFDRole = LFDRole
 -----------------------------------------------------------------------------
--- powervalue
------------------------------------------------------------------------------
---	local power = CreateFrame("StatusBar", nil, self)
---	self.Power = power
---	power.value = SetFontString(health)
---	power.value:SetPoint("LEFT", self.Panel, "LEFT", 4, 0);
---	power.PreUpdate = PreUpdatePower
---	power.PostUpdate = PostUpdatePower
------------------------------------------------------------------------------
 -- Name
 -----------------------------------------------------------------------------
-	local Name = T.SetFontString(health)
-		Name:SetPoint("CENTER", self.Panel)
-		Name:SetJustifyH("LEFT")
-		Name:SetFont(C["media"].font, C["media"].fontsizenorm, C["media"].fonttnoutline);
-		self:Tag(Name, '[getnamecolor][namesupershort][shortclassification] [level]')
-		self.Name = Name
+local Name = T.SetFontString(health)
+	Name:SetPoint("CENTER", self.Panel)
+	Name:SetJustifyH("LEFT")
+	Name:SetFont(C["media"].font, C["media"].fontsizenorm, C["media"].fonttnoutline);
+	self:Tag(Name, '[getnamecolor][namesupershort][shortclassification] [level]')
+	self.Name = Name
 -----------------------------------------------------------------------------
 -- Raidicons
 -----------------------------------------------------------------------------
-	local RaidIcon = health:CreateTexture(nil, 'OVERLAY')
-		RaidIcon:SetHeight(18)
-		RaidIcon:SetWidth(18)
-		RaidIcon:SetPoint('RIGHT', -2, -1)
-		self.RaidIcon = RaidIcon
+local RaidIcon = health:CreateTexture(nil, 'OVERLAY')
+	RaidIcon:SetHeight(18)
+	RaidIcon:SetWidth(18)
+	RaidIcon:SetPoint('RIGHT', -2, -1)
+	self.RaidIcon = RaidIcon
 -----------------------------------------------------------------------------
 -- Register targetchange
 -----------------------------------------------------------------------------
-
-	self:RegisterEvent("PLAYER_TARGET_CHANGED", function(self)
-		if ( UnitIsUnit("target", self.unit) ) then
-			self.Border:Show();
-		else
-			self.Border:Hide();
-		end
-	end);
+self:RegisterEvent("PLAYER_TARGET_CHANGED", function(self)
+	if ( UnitIsUnit("target", self.unit) ) then
+		self.Border:Show();
+	else
+		self.Border:Hide();
+	end
+end);
 -----------------------------------------------------------------------------
 -- Debuffhighlight
 -----------------------------------------------------------------------------	
-	--CreateHighlight(self)
-	local dbh = hp:CreateTexture(nil, "OVERLAY")
-		dbh:SetWidth(32)
-		dbh:SetHeight(32)
-		dbh:SetPoint("CENTER", self, "CENTER")
-		self.DebuffHighlightUseTexture = true
-		self.DebuffHighlight = dbh
+--CreateHighlight(self)
+local dbh = health:CreateTexture(nil, "OVERLAY")
+	dbh:SetWidth(32)
+	dbh:SetHeight(32)
+	dbh:SetPoint("CENTER", self, "CENTER")
+	self.DebuffHighlightUseTexture = true
+	self.DebuffHighlight = dbh
 -----------------------------------------------------------------------------
 -- Range
 -----------------------------------------------------------------------------
-	local range = {insideAlpha = 1, outsideAlpha = 0.3}
-		self.Range = range
+local range = {insideAlpha = 1, outsideAlpha = 0.3}
+	self.Range = range
 -----------------------------------------------------------------------------
 -- Healcomm
 -----------------------------------------------------------------------------
@@ -239,8 +215,7 @@ local function Shared(self, unit)
 -- Aurawatch
 -----------------------------------------------------------------------------
 	T.createAuraWatch(self)
-
-    return self
+	return self
 end
 
 oUF:RegisterStyle('Party', Shared)
@@ -318,10 +293,8 @@ oUF:SetActiveStyle("Party")
 	GroupAnchor:SetPoint("BOTTOMLEFT", NevermoreCenter, "TOPLEFT", T.buttonspacing, -(NevermoreCenter:GetHeight() + (iPlayer:GetHeight() * 1.5))) 
 	NeHParty:SetPoint("TOPLEFT", GroupAnchor, 0, 0)
 end)
-
-
-			end
-			if k == "role" and v == "tank" then  -- Checks to see if we're tank
+	end
+if k == "role" and v == "tank" then  -- Checks to see if we're tank
 oUF:Factory(function(self)
 
 oUF:SetActiveStyle("Party")	
@@ -353,7 +326,7 @@ oUF:SetActiveStyle("Party")
 	GroupAnchor:SetWidth(500)
 	GroupAnchor:SetPoint("TOPLEFT", NevermoreInfo, "TOPLEFT", T.buttonspacing, -T.buttonspacing) 
 	NeTParty:SetPoint("TOPLEFT", GroupAnchor, 0, 0)
-end)
+	end)
  			end
 		end
 	end
